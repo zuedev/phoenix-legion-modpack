813a930aabc2c04e7955b30fcb4a2ce3e4d87b16
zuedev <zuedev@gmail.com>
2024-11-08 10:28:50 +0000

    store hemtt exe path too

--- release.ps1
+++ release.ps1
@@ -1,9 +1,12 @@
 # store location of Arma 3 Tools PublisherCmd
 $PublisherCMD = "C:\Program Files (x86)\Steam\steamapps\common\Arma 3 Tools\Publisher\PublisherCmd.exe"
 
+# store location of hemtt executable
+$HEMTT = "C:\Users\zuedev\git\github.com\zuedev\HEMTT_Devcontainer\target\x86_64-pc-windows-gnu\release\hemtt.exe"
+
 # run HEMTT release if mod has a .hemtt/project.toml file
 if (Test-Path ".hemtt\project.toml") {
-    hemtt release
+    $HEMTT release
 
     # use Arma 3 Tools to publish if mod has a steam\workshop_id.txt file
     if (Test-Path "steam\workshop_id.txt") {
