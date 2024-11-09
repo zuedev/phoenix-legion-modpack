a2a2255ab3c591d89dbfa6fe22537109dadee156
zuedev <zuedev@gmail.com>
2024-11-09 16:29:57 +0000

    improve release script to include version number in commit message

--- release.ps1
+++ release.ps1
@@ -4,6 +4,8 @@ $PublisherCMD = "C:\Program Files (x86)\Steam\steamapps\common\Arma 3 Tools\Publ
 # store location of hemtt executable
 $HEMTT = "C:\Users\zuedev\git\github.com\zuedev\HEMTT_Devcontainer\target\x86_64-pc-windows-gnu\release\hemtt.exe"
 
+$PROJECT_VERSION = & $HEMTT value project.version
+
 # run HEMTT release if mod has a .hemtt/project.toml file
 if (Test-Path ".hemtt\project.toml") {
     & $HEMTT release
@@ -13,6 +15,6 @@ if (Test-Path ".hemtt\project.toml") {
         # read workshop id from file
         $workshopId = Get-Content "steam\workshop_id.txt"
 
-        & $PublisherCMD update /id:$workshopId /path:.hemttout\release\ /changeNote:"automated release"
+        & $PublisherCMD update /id:$workshopId /path:.hemttout\release\ /changeNote:"automated release for version $PROJECT_VERSION"
     }
 }
